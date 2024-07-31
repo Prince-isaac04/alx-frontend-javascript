6e8fce671391f2d09d1cd655137ca26ad5f3653e
Prince-isaac04 <ajayioluwaseyi1692002@gmail.com>
2024-07-31 10:54:23 +0100

    add

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,6 @@
 export default function getListStudentIds(list) {
-  // Check if the input is not an array
   if (!Array.isArray(list)) {
     return [];
   }
-  
-  // Use the map function to extract the IDs
   return list.map((obj) => obj.id);
 }
