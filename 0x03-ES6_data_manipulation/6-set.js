1909d35f813eb9048e2f6047d2ce094693e899d2
Prince-isaac04 <ajayioluwaseyi1692002@gmail.com>
2024-08-01 18:34:17 +0100

    datamanipulation Es6

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,5 +1,7 @@
 export default function setFromArray(array) {
-  if (Array.isArray(array)) {
-    return new Set(array)
+  if (!Array.isArray(array)) {
+    console.log('The provided argument is not an array');
+    return null;
   }
+  return new Set(array);
 }
