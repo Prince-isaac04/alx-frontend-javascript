59a0308813ccf1b22b192e96548618a8b68c64e1
Prince-isaac04 <ajayioluwaseyi1692002@gmail.com>
2024-08-01 19:01:00 +0100

    read

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,8 @@
 export default function cleanSet(set, startString) {
   const result = Array.from(set)
-    .filter(value => value.startsWith(startString))
-    .map(value => value.slice(startString.length))
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
     .join('-');
-  
+
   return result;
 }
-
